59c3a025e7b3a3e65fa733b2e847e9372b17317f
changev <alan.wei@emc.com>
2016-11-17 17:43:04 +0800

    pip install ansible && aptitude install rackhd

--- packer/scripts/dep.sh
+++ packer/scripts/dep.sh
@@ -4,7 +4,8 @@
 apt-get -y update
 
 # to support using the ansible provisioner
-apt-get -y install ansible
+apt-get -y install expect python-dev python-pip python-yaml libffi-dev 
+pip install ansible==2.2.0.0
 
 # set a friendly hostname
 rm -f /etc/hostname
