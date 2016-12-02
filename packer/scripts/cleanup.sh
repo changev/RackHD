7bc813eed8dd0371f26346c657ef9906f68fb5be
changev <alan.wei@emc.com>
2016-12-02 16:51:58 +0800

    f

--- packer/scripts/cleanup.sh
+++ packer/scripts/cleanup.sh
@@ -24,7 +24,7 @@ rm /var/lib/dhcp/*
 # Make sure Udev doesn't block our network
 echo "cleaning up udev rules"
 rm -f /etc/udev/rules.d/70-persistent-net.rules
-mkdir /etc/udev/rules.d/70-persistent-net.rules
+#mkdir /etc/udev/rules.d/70-persistent-net.rules
 rm -rf /dev/.udev/
 rm -f /lib/udev/rules.d/75-persistent-net-generator.rules
 
